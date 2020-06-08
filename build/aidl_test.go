7b94c197afe92a2cccbcf97b989dc7b23a4dcc9f
Colin Cross <ccross@android.com>
2020-06-07 18:04:10 -0700

    Follow Soong changes to PathForSource behavior in tests
    Soong no longer enforces that files referenced with PathForSource
    or PathForModuleSrc exist in the mock filesystem.  Remove unnecessary
    files from the AIDL tests, and manually throw an error for the API
    files since the test wants to assert they don't exist.
    
    Bug: 153485543
    Test: aidl_test.go
    Change-Id: I59018e2d69257ac6ce7d2c0b5e3e3fc5f18702fb
    Merged-In: I59018e2d69257ac6ce7d2c0b5e3e3fc5f18702fb
    (cherry picked from commit 1976ae24f4d2f491f213f1653b25423fc9253ce4)

--- build/aidl_test.go
+++ build/aidl_test.go
@@ -104,17 +104,7 @@ func _testAidl(t *testing.T, bp string, customizers ...testCustomizer) (*android
 			name: "aidl_metadata_json",
 		}
 	`
-	fs := map[string][]byte{
-		"a.java":              nil,
-		"AndroidManifest.xml": nil,
-		"build/make/target/product/security/testkey": nil,
-		"framework/aidl/a.aidl":                      nil,
-		"IFoo.aidl":                                  nil,
-		"libbinder_ndk.map.txt":                      nil,
-		"system/tools/aidl/build/message_check_compatibility.txt": nil,
-		"system/tools/aidl/build/message_check_equality.txt":      nil,
-		"system/tools/aidl/build/message_check_integrity.txt":     nil,
-	}
+	fs := map[string][]byte{}
 
 	cc.GatherRequiredFilesForTest(fs)
 
@@ -246,12 +236,10 @@ func TestUnstableVersionUsageInRelease(t *testing.T) {
 	expectedError := `unstable-java is disallowed in release version because it is unstable.`
 	testAidlError(t, expectedError, unstableVersionUsageInJavaBp, setReleaseEnv(), withFiles(map[string][]byte{
 		"aidl_api/foo/1/foo.1.aidl": nil,
-		"aidl_api/foo/1/.hash":      nil,
 	}))
 
 	testAidl(t, unstableVersionUsageInJavaBp, withFiles(map[string][]byte{
 		"aidl_api/foo/1/foo.1.aidl": nil,
-		"aidl_api/foo/1/.hash":      nil,
 	}))
 
 	// A stable version can be used in release version
@@ -272,12 +260,10 @@ func TestUnstableVersionUsageInRelease(t *testing.T) {
 
 	testAidl(t, stableVersionUsageInJavaBp, setReleaseEnv(), withFiles(map[string][]byte{
 		"aidl_api/foo/1/foo.1.aidl": nil,
-		"aidl_api/foo/1/.hash":      nil,
 	}))
 
 	testAidl(t, stableVersionUsageInJavaBp, withFiles(map[string][]byte{
 		"aidl_api/foo/1/foo.1.aidl": nil,
-		"aidl_api/foo/1/.hash":      nil,
 	}))
 }
 
@@ -395,7 +381,6 @@ func TestCreatesModulesWithFrozenVersions(t *testing.T) {
 		}
 	`, withFiles(map[string][]byte{
 		"aidl_api/foo/1/foo.1.aidl": nil,
-		"aidl_api/foo/1/.hash":      nil,
 	}))
 
 	// For alias for the latest frozen version (=1)
@@ -457,9 +442,7 @@ func TestNativeOutputIsAlwaysVersioned(t *testing.T) {
 		}
 	`, withFiles(map[string][]byte{
 		"aidl_api/foo/1/foo.1.aidl": nil,
-		"aidl_api/foo/1/.hash":      nil,
 		"aidl_api/foo/2/foo.2.aidl": nil,
-		"aidl_api/foo/2/.hash":      nil,
 	}))
 
 	// alias for the latest frozen version (=2)
@@ -543,9 +526,7 @@ func TestImports(t *testing.T) {
 				},
 			},
 		}
-	`, withFiles(map[string][]byte{
-		"IBar.aidl": nil,
-	}))
+	`)
 
 	testAidlError(t, `backend.cpp.enabled: C\+\+ backend not enabled in the imported AIDL interface "bar"`, `
 		aidl_interface {
@@ -568,9 +549,7 @@ func TestImports(t *testing.T) {
 				},
 			},
 		}
-	`, withFiles(map[string][]byte{
-		"IBar.aidl": nil,
-	}))
+	`)
 
 	ctx, _ := testAidl(t, `
 		aidl_interface {
@@ -588,9 +567,7 @@ func TestImports(t *testing.T) {
 				"IBar.aidl",
 			],
 		}
-	`, withFiles(map[string][]byte{
-		"IBar.aidl": nil,
-	}))
+	`)
 
 	ldRule := ctx.ModuleForTests("foo-cpp", nativeVariant).Rule("ld")
 	libFlags := ldRule.Args["libFlags"]
